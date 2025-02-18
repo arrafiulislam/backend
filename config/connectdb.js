97a0b778b0c8aaa0ab9fb0b7fc80c131661f8000
arrafiulislam <arrafiulislamrafim@gmail.com>
2025-02-18 17:02:57 +0600

    asd

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async(DATABASE_URL) => {
-    try {
-        const DB_OPTIONS = {
-            dbname: "Spectalyzer"
-        }
-        await mongoose.connect(DATABASE_URL, DB_OPTIONS)
-        console.log('Connected Successfully')
-    } catch (error) {
-        console.log('Error while connecting to database')
-    }
-}
+const connectDB = async (DATABASE_URL) => {
+  try {
+    const DB_OPTIONS = {
+      dbname: "Spectalyzer",
+    };
+    await mongoose.connect(DATABASE_URL, DB_OPTIONS);
+    console.log("Connected Successfully");
+  } catch (error) {
+    console.log("Error while connecting to database");
+  }
+};
 
-export default connectDB
+export default connectDB;
